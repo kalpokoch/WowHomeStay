395cb8b589c7002fbb16fd82b837e8d11e73b57c
kalpokoch <kalpokoch@gmail.com>
2025-08-22 16:58:05 +0530

    fixed show all photos button

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -27,55 +27,55 @@ const ImageGallery = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-2 h-[300px] md:h-[400px] rounded-xl overflow-hidden mb-8">
-      {/* Main large image */}
-      <div 
-        className="md:col-span-2 relative group cursor-pointer"
-        onMouseEnter={() => setHoveredImage(1)}
-        onMouseLeave={() => setHoveredImage(null)}
-      >
-        <img
-          src={images[0].src}
-          alt={images[0].alt}
-          className={`w-full h-full object-cover transition-all duration-300 ${
-            hoveredImage === 1 ? "brightness-90 scale-105" : ""
-          }`}
-        />
-      </div>
+    <div className="relative mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-2 h-[300px] md:h-[400px] rounded-xl overflow-hidden">
+        {/* Main large image */}
+        <div 
+          className="md:col-span-2 relative group cursor-pointer"
+          onMouseEnter={() => setHoveredImage(1)}
+          onMouseLeave={() => setHoveredImage(null)}
+        >
+          <img
+            src={images[0].src}
+            alt={images[0].alt}
+            className={`w-full h-full object-cover transition-all duration-300 ${
+              hoveredImage === 1 ? "brightness-90 scale-105" : ""
+            }`}
+          />
+        </div>
 
-      {/* Grid of smaller images */}
-      <div className="md:col-span-2 grid grid-cols-2 gap-2">
-        {images.slice(1).map((image, index) => (
-          <div
-            key={image.id}
-            className="relative group cursor-pointer"
-            onMouseEnter={() => setHoveredImage(image.id)}
-            onMouseLeave={() => setHoveredImage(null)}
-          >
-            <img
-              src={image.src}
-              alt={image.alt}
-              className={`w-full h-full object-cover transition-all duration-300 ${
-                hoveredImage === image.id ? "brightness-90 scale-105" : ""
-              }`}
-            />
-            
-            {/* Show all photos button on last image */}
-            {index === 3 && (
-              <div className="absolute inset-0 bg-black/30 flex items-end justify-end p-4">
-                <Button
-                  variant="outline"
-                  size="sm"
-                  onClick={handleShowAllPhotos}
-                  className="bg-white text-airbnb-gray border-airbnb-gray-light hover:shadow-elegant z-10"
-                >
-                  <LayoutGrid className="h-4 w-4 mr-2" />
-                  Show all photos
-                </Button>
-              </div>
-            )}
-          </div>
-        ))}
+        {/* Grid of smaller images */}
+        <div className="md:col-span-2 grid grid-cols-2 gap-2">
+          {images.slice(1).map((image, index) => (
+            <div
+              key={image.id}
+              className="relative group cursor-pointer"
+              onMouseEnter={() => setHoveredImage(image.id)}
+              onMouseLeave={() => setHoveredImage(null)}
+            >
+              <img
+                src={image.src}
+                alt={image.alt}
+                className={`w-full h-full object-cover transition-all duration-300 ${
+                  hoveredImage === image.id ? "brightness-90 scale-105" : ""
+                }`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      {/* Show all photos button positioned outside the grid */}
+      <div className="absolute bottom-4 right-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={handleShowAllPhotos}
+          className="bg-white text-airbnb-gray border-airbnb-gray-light hover:shadow-elegant z-10"
+        >
+          <LayoutGrid className="h-4 w-4 mr-2" />
+          Show all photos
+        </Button>
       </div>
     </div>
   );
